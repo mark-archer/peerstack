8d3dcbd657ba8d9ff534e2c61f014cb8c17cd502
Mark Archer <marka@ifit.com>
2020-12-29 14:55:41 -0800

    prep for npm publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello world');
+export * as common from './common';
+export * as connectionsServer from './connections-server'
+export * as connections from './connections'
+export * as db from './db'
+export * as fingerprint from './fingerprint'
+export * as remoteCalls from './remote-calls'
+export * as user from './user'
