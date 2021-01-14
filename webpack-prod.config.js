db411bbd3dfce1d50213a65847041ab30297ff80
Mark Archer <marka@ifit.com>
2021-01-13 19:58:45 -0800

    use webpack to compile js to a single file

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,38 +1,40 @@
 var CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
-  
+  entry: './src/index.ts',
+
   mode: 'production',
-  
+
   output: {
-    filename: 'index.bundle.js',
-    path: __dirname + '/public'
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    library: 'peerstack',
+    libraryTarget: 'umd'
   },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-  
+
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
-      // svg loader
-      { test: /\.svg$/, loader: 'svg-inline-loader' },
+      // // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      // { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
-      // css loader
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
-      }
+      // // svg loader
+      // { test: /\.svg$/, loader: 'svg-inline-loader' },
+
+      // // css loader
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     { loader: "style-loader" },
+      //     { loader: "css-loader" }
+      //   ]
+      // }
     ]
   },
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,28 @@ var webpack = require('webpack');
 var CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/index.ts',
 
   mode: 'development',
   watch: true,
   devtool: 'source-map',
-  
+
   output: {
-    filename: 'index.bundle.js',
-    path: __dirname + '/public'
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    library: 'peerstack',
+    libraryTarget: 'umd'
   },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-  
+
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      
+
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
