f98f772c4fe63cf8544351031c82cc9003b05747
Mark Archer <marka@ifit.com>
2021-03-29 21:18:59 -0700

    add dev bundle

---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+// var CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+
+  mode: 'development',
+
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.bundle.dev.js',
+    library: 'peerstack',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    ]
+  },
+
+  plugins: [
+    // new CompressionPlugin() // gzips the bundle
+  ],
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "lodash": "_"
+  }
+};
